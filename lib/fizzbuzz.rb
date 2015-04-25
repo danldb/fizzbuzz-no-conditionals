67c1a5aa88efe43eebec4b96e48b782f32b038d9
Daniel Le Dosquet-Bergquist <dpbergquist@gmail.com>
2015-04-25 07:57:43 +0100

    refactor play method for single responsibility

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,14 +5,15 @@ class FizzBuzz
   RULES = {'15' => 'FizzBuzz', '5' => 'Buzz', '3' => 'Fizz'}
 
   def play(number)
-    @responses = number.denominators.map {|n| RULES[n.to_s]}
-    response || number
+    @number = number
+    return response || number
   end
 
   private
 
   def response
-    @responses.select {|n| n != nil}.last
+    responses = @number.denominators.map {|n| RULES[n.to_s]}
+    responses.select {|n| n != nil}.last
   end
 
 end
